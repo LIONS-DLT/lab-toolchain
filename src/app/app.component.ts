24bcd93e24fb418acbdef229d9974ce1715464ab
KaS <karlseidenfad@yahoo.de>
2021-12-03 10:19:21 +0100

    installing mqttjs and types for ws; sizing the widget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
-// import * as mqtt from 'mqtt';
+import * as mqtt from 'mqtt';
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
 // import am5geodata_worldLow from "@amcharts/amcharts5/geodata/worldLow";
