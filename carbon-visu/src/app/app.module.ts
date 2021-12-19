e9b706fe3260f5adc74f5642e1cd702d4ac6d55a
KaS <karlseidenfad@yahoo.de>
2021-12-19 10:31:48 +0100

    modified tsconfig, created new barchart component, scaling visu for larger screens

--- carbon-visu/src/app/app.module.ts
+++ carbon-visu/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CapAndTradeWidgetComponent } from './_components/cap-and-trade-widget/cap-and-trade-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapAndTradeWidgetComponent
   ],
   imports: [
     BrowserModule
